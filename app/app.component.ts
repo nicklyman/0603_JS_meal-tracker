b48a958e026e8ddc67383120d00f4b5900e338e6
Nick <nick.lyman@gmail.com>
2016-06-03 09:19:42 -0700

    Add a Meal class with an array of meals including name, description, calories, and unique ID. Display the list of meal names on the page, and display all values for each meal in the console log.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Meal Tracker App!</h1>
+    <div class="container">
+      <h1>Meal Tracker</h1>
+      <h3 *ngFor="#meal of meals" (click)="mealWasSelected(meal)">
+        {{ meal.name }}
+      </h3>
+    </div>
   `
 })
 export class AppComponent {
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Bagel and cream cheese", "Light cream cheese", 410, 0),
+      new Meal("Hamburger", "Added bacon and mushrooms", 775, 1),
+      new Meal("Gin and juice", "Grapefruit juice", 300, 2),
+      new Meal("Salad", "Fresh vegetables from the garden", 330, 3),
+      new Meal("Banana split", "Worth every bite!", 900, 4)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+  }
+}
+
+export class Meal {
+  constructor(public name: string, public description: string, public calories: number, public id: number) {
+
+  }
 }
